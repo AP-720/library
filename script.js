a1474d0238d806ee1dcc17983a4e56619817b065
Andy Price <andyprice720@gmail.com>
2024-09-17 21:30:50 +0100

    Create Add Book Function
    Wrote function to add book to the library array. Wrote function to display
    books in library. Basic styling too page and cards.

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
-function book(title, author, pages, read) {
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
-        return this.title + this.author + this.pages + this.read;
-    }
 }
 
-const theHobbit = new book('The Hobbit', ' by J.R.R. Tolkien,', ' 295 pages,', ' not read yet');
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook); 
+}
 
-// // console.log(theHobbit.info())
+myLibrary.push(new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', false));
+myLibrary.push(new Book('Ulysses', 'James Joyce', '736 pages', true));
+myLibrary.push(new Book('Crime and Punishment', 'Fyodor Dostoevsky', '720 pages', true));
+
+function displayBooks() {
+    const bookContainer = document.querySelector('.book-container');
+    bookContainer.innerHTML = "";
+
+    myLibrary.forEach((book, index) => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        card.innerHTML = `
+        <h2>${book.title}</h2>
+        <p><strong>Author:</strong> ${book.author}</p>
+        <p><strong>Pages:</strong> ${book.pages}</p>
+        <p><strong>Read:</strong> ${book.read ? "Yes" : "No"}</p>
+      `;
+
+      bookContainer.appendChild(card);
+});
+}
+
+displayBooks();
+
+
