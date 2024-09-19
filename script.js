011bbfcfac07eac04cc2ee56e575bdfdd0b979a1
Andy Price <andyprice720@gmail.com>
2024-09-19 21:30:23 +0100

    Create Add Book & Cancel Functionality
    Created event listener to add new book to library. Had to figure out
    how to get the correct value from the checkbox, needed to use .checked.
    For cancel, had to reset the form and close modal.

--- script.js
+++ script.js
@@ -4,10 +4,7 @@ const newBook = document.querySelector("[data-new-book]");
 const modal = document.querySelector("[data-modal]");
 const cancelButton = document.querySelector("[data-cancel]");
 const addButton = document.querySelector("[data-add]");
-
-newBook.addEventListener("click", () => {
-    modal.showModal();
-});
+const addBookForm = document.getElementById("add-book-form");
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -20,12 +17,6 @@ function addBookToLibrary(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook); 
 }
-
-myLibrary.push(new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', false));
-myLibrary.push(new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', false));
-myLibrary.push(new Book('Ulysses', 'James Joyce', '736 pages', true));
-myLibrary.push(new Book('Ulysses', 'James Joyce', '736 pages', true));
-myLibrary.push(new Book('Crime and Punishment', 'Fyodor Dostoevsky', '720 pages', true));
 
 function displayBooks() {
     const bookContainer = document.querySelector('.book-container');
@@ -45,6 +36,36 @@ function displayBooks() {
       bookContainer.appendChild(card);
 });
 }
+
+newBook.addEventListener("click", () => {
+    modal.showModal();
+});
+
+cancelButton.addEventListener("click", () => {
+    document.getElementById("add-book-form").reset();
+    modal.close();
+});
+
+addBookForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let read = document.getElementById("read").checked;
+
+    console.log(read);
+
+    addBookToLibrary(title, author, pages, read);
+    displayBooks();
+    document.getElementById("add-book-form").reset();
+    modal.close();
+});
+
+// myLibrary.push(new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', false));
+myLibrary.push(new Book('Ulysses', 'James Joyce', '736 pages', true));
+myLibrary.push(new Book('Crime and Punishment', 'Fyodor Dostoevsky', '720 pages', true));
+
 
 displayBooks();
 
