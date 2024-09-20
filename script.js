34eb12343970a98fe7c0372f5aeacdf2cef0794b
Andy Price <andyprice720@gmail.com>
2024-09-20 20:40:50 +0100

    Add Remove Book Functionality
    Wrote function to add event listener to remove button which deletes
    book from library array as well as removed it from the DOM. Linked the
    book card to the library by assigning the array index as a data attribute.
    Got stuck on the fact needed to assign it to every button each time using
    querySelectorAll and then forEach.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const newBook = document.querySelector("[data-new-book]");
 const modal = document.querySelector("[data-modal]");
 const cancelButton = document.querySelector("[data-cancel]");
 const addButton = document.querySelector("[data-add]");
-const addBookForm = document.getElementById("add-book-form");
+const addBookBtn = document.getElementById("add-book-form");
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -25,17 +25,37 @@ function displayBooks() {
     myLibrary.forEach((book, index) => {
         const card = document.createElement('div');
         card.classList.add('card');
+        card.setAttribute("data-book-num", index)
 
         card.innerHTML = `
         <h2>${book.title}</h2>
         <p><strong>Author:</strong> ${book.author}</p>
         <p><strong>Pages:</strong> ${book.pages}</p>
         <p><strong>Read:</strong> ${book.read ? "Yes" : "No"}</p>
+        <div class="card-btn"> 
+        <button type="button" class="remove-btn">Remove</button>
+        <button type="button" class="update-read-btn">Read</button>
+        </div>
       `;
 
       bookContainer.appendChild(card);
 });
 }
+
+function addRemoveBookListener(button) {
+    button.addEventListener("click", (event) => {
+        const selectedCard = event.target.closest('.card');
+        
+        let bookNumber = selectedCard.getAttribute("data-book-num");
+        console.log(bookNumber);
+        
+        myLibrary.splice(`${bookNumber}`, 1)
+
+        console.table(myLibrary);
+
+        selectedCard.remove();            
+});
+};
 
 newBook.addEventListener("click", () => {
     modal.showModal();
@@ -46,7 +66,7 @@ cancelButton.addEventListener("click", () => {
     modal.close();
 });
 
-addBookForm.addEventListener("submit", (e) => {
+addBookBtn.addEventListener("submit", (e) => {
     e.preventDefault();
 
     let title = document.getElementById("title").value;
@@ -54,13 +74,33 @@ addBookForm.addEventListener("submit", (e) => {
     let pages = document.getElementById("pages").value;
     let read = document.getElementById("read").checked;
 
-    console.log(read);
-
     addBookToLibrary(title, author, pages, read);
     displayBooks();
+    const removeButton = document.querySelectorAll('.remove-btn');
+    removeButton.forEach(button => addRemoveBookListener(button));
     document.getElementById("add-book-form").reset();
     modal.close();
 });
+
+
+
+// Remove Book and Update Read Buttons for the preloaded books
+
+window.onload=function() {
+    const removeBook = document.querySelectorAll(".remove-btn");
+
+    removeBook.forEach(button => {
+        addRemoveBookListener(button);
+    })
+};
+
+// const updateRead = document.querySelectorAll(".update-read-btn");
+// updateRead.forEach(button => {
+//     button.addEventListener("click", () => {
+//         console.log('update')
+//     });
+// });
+
 
 // myLibrary.push(new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', false));
 myLibrary.push(new Book('Ulysses', 'James Joyce', '736 pages', true));
