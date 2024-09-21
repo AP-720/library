2a7f1977fa3f8d0235916ca1c5e04f2d2caead34
Andy Price <andyprice720@gmail.com>
2024-09-21 21:31:54 +0100

    Create Read Status Toggle
    Wrote function to change the read status of books when button clicked.
    Both in the object and the text content of the card. Had issues when
    accessing the text, was using event.target.closest which selects the
    parent element, when I needed a sibling so used .querySelector instead.

--- script.js
+++ script.js
@@ -13,6 +13,10 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+Book.prototype.toggleRead = function() {
+    this.read = !this.read;
+}
+
 function addBookToLibrary(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook); 
@@ -31,10 +35,10 @@ function displayBooks() {
         <h2>${book.title}</h2>
         <p><strong>Author:</strong> ${book.author}</p>
         <p><strong>Pages:</strong> ${book.pages}</p>
-        <p><strong>Read:</strong> ${book.read ? "Yes" : "No"}</p>
+        <p><strong>Read:</strong> <span class="read-state">${book.read ? "Yes" : "No"}</span></p>
         <div class="card-btn"> 
         <button type="button" class="remove-btn">Remove</button>
-        <button type="button" class="update-read-btn">Read</button>
+        <button type="button" class="update-read-btn">Toggle Read</button>
         </div>
       `;
 
@@ -47,15 +51,29 @@ function addRemoveBookListener(button) {
         const selectedCard = event.target.closest('.card');
         
         let bookNumber = selectedCard.getAttribute("data-book-num");
-        console.log(bookNumber);
-        
         myLibrary.splice(`${bookNumber}`, 1)
-
-        console.table(myLibrary);
-
+     
         selectedCard.remove();            
 });
 };
+
+function updateRead(button) {
+    button.addEventListener("click", (event) => {
+        const selectedCard = event.target.closest('.card');
+        const readText = selectedCard.querySelector('.read-state')
+        
+        let bookNumber = selectedCard.getAttribute("data-book-num");
+        myLibrary[bookNumber].toggleRead();
+
+        switch(readText.textContent) {
+            case "Yes":
+                readText.textContent = "No";
+                break;
+            case "No":
+                readText.textContent = "Yes";
+        }
+    })
+}
 
 newBook.addEventListener("click", () => {
     modal.showModal();
@@ -78,29 +96,28 @@ addBookBtn.addEventListener("submit", (e) => {
     displayBooks();
     const removeButton = document.querySelectorAll('.remove-btn');
     removeButton.forEach(button => addRemoveBookListener(button));
+
+    const toggleRead = document.querySelectorAll(".update-read-btn");
+    toggleRead.forEach(button => updateRead(button));
+
     document.getElementById("add-book-form").reset();
     modal.close();
 });
-
-
 
 // Remove Book and Update Read Buttons for the preloaded books
 
 window.onload=function() {
     const removeBook = document.querySelectorAll(".remove-btn");
-
+    const toggleRead = document.querySelectorAll(".update-read-btn");
+    
     removeBook.forEach(button => {
         addRemoveBookListener(button);
     })
+
+    toggleRead.forEach(button => {
+        updateRead(button);
+    })
 };
-
-// const updateRead = document.querySelectorAll(".update-read-btn");
-// updateRead.forEach(button => {
-//     button.addEventListener("click", () => {
-//         console.log('update')
-//     });
-// });
-
 
 // myLibrary.push(new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', false));
 myLibrary.push(new Book('Ulysses', 'James Joyce', '736 pages', true));
